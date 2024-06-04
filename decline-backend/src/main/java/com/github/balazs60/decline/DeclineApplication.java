45e5a5faa00ba32acbd7aed41064223af9248c7a
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2024-06-04 13:52:56 +0200

    Set up spring boot and dependencies

---
+++ decline-backend/src/main/java/com/github/balazs60/decline/DeclineApplication.java
@@ -0,0 +1,13 @@
+package com.github.balazs60.decline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclineApplication.class, args);
+	}
+
+}
---
+++ decline-backend/src/test/java/com/github/balazs60/decline/DeclineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.balazs60.decline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
