d53e7322dd256641e37f962b3099000433e4da0c
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2024-06-14 15:41:24 +0200

    Add checkbox to select answer to adjectives and articles on task page

--- decline-frontend/src/app/task/task.component.ts
+++ decline-frontend/src/app/task/task.component.ts
@@ -4,11 +4,13 @@ import { Task } from '../../types';
 import { CommonModule } from '@angular/common';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';  // Import FormsModule
+
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
@@ -22,11 +24,19 @@ export class TaskComponent {
     adjectiveAnswerOptions: []
   }
 
-  constructor(private taskService: TasksService) {}
+  selectedArticle: string;
+  selectedAdjective: string;
+
+  constructor(private taskService: TasksService) {
+    this.selectedAdjective = "",
+    this.selectedArticle=""
+  }
 
   ngOnInit() {
     console.log("ngOnInit");
     this.taskService.getTask('/api/task').subscribe((task: Task) => {
+      this.selectedArticle = '';
+      this.selectedAdjective = '';
       console.log(task.task);
       this.task = task
     });
