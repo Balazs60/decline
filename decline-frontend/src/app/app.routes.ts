2fc103221675adb679b547eddc4158b944137a24
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2024-07-03 17:28:04 +0200

    Add header and about pages

--- decline-frontend/src/app/app.routes.ts
+++ decline-frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TaskComponent } from './task/task.component';
+import { AboutComponent } from './about/about/about.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: TaskComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
       }
 ];
