a58b56e54fde9bc595252489f57a17e5b2449384
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2024-07-09 15:56:49 +0200

    Don't show profile button on header page when user logged out.

--- decline-frontend/src/app/header/header/header.component.ts
+++ decline-frontend/src/app/header/header/header.component.ts
@@ -14,6 +14,7 @@ import { LoginService } from '../../services/login.service';
 export class HeaderComponent {
 
    isLoggedIn: boolean = false;
+   
 
    constructor(private router: Router,private loginService: LoginService) {}
 
